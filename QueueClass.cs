7c3e05f5d69e6561be734cdc26fb62480c5fd164
Paolo Pinca <164370429+Wujipao@users.noreply.github.com>
2024-10-11 14:26:01 +0800

    Add files via upload

---
+++ QueueClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class QueueClass<T>
+    {
+        private T[] arr;
+        private int count;
+        private int size;
+        private int front;
+
+        public QueueClass(int size)
+        {
+            this.arr = new T[size];
+            this.size = size;
+            this.count = 0;
+            this.front = 0;
+        }
+        public bool ifEmpty()
+        {
+            return count == 0;
+        }
+        public bool ifFull()
+        {
+            return count == size;
+        }
+        public T Enqueue(T item)
+        {
+            if (ifFull())
+            {
+                throw new Exception("Queue is Full.");
+            }
+            arr[count] = item;
+            count++;
+            return item;
+        }
+        public T Dequeue()
+        {
+            if (ifEmpty())
+            {
+                throw new Exception("Queue is empty.");
+            }
+            T dequeued = arr[front];
+            arr[front] = default;
+            front = (front + 1) % size;
+            count--;
+            return dequeued;
+        }
+        public void Display()
+        {
+            if (ifEmpty())
+            {
+                Console.WriteLine("Queue is empty.");
+                return;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                int index = (front + i) % size;
+                Console.Write(arr[index] + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ QueueTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class QueueTest
+    {
+        static void Main(string[] args)
+        {
+            QueueClass<int> q = new QueueClass<int>(5);
+
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Enqueue(4);
+            q.Enqueue(5);
+
+            q.Dequeue();
+            q.Dequeue();
+
+            q.Display();
+        }
+    }
+}
